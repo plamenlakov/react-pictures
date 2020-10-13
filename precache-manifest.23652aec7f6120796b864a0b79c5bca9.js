479f27bbf91c16353e7cb926baac8b8cab1a9aca
plamenlakov <frainbreeze3@gmail.com>
2020-10-13 15:23:07 +0200

    Updates

---
+++ precache-manifest.23652aec7f6120796b864a0b79c5bca9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeba62a3b221e67dff320911c8ea91ab",
+    "url": "/react-pictures/index.html"
+  },
+  {
+    "revision": "5a0746e63f59fd2b8088",
+    "url": "/react-pictures/static/css/main.3379a098.chunk.css"
+  },
+  {
+    "revision": "83780c69b5c5574b9273",
+    "url": "/react-pictures/static/js/2.e752a839.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-pictures/static/js/2.e752a839.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a0746e63f59fd2b8088",
+    "url": "/react-pictures/static/js/main.f09a9ec7.chunk.js"
+  },
+  {
+    "revision": "4004570af0434757ba74",
+    "url": "/react-pictures/static/js/runtime-main.72b57ca4.js"
+  }
+]);
